abc0396b0109b56617ee77ed1b61bb740209639e
kzw <kzw@google.com>
2023-05-26 01:31:03 +0200

    update 6

--- source/main.py
+++ source/main.py
@@ -1,4 +1,4 @@
 print("*****************************")
 print("Hello Google Cloud and World!")
 print("*****************************")
-print("by @kzw - May 2023 - Update 5")
+print("by @kzw - May 2023 - Update 6")
